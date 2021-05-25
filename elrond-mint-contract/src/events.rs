4b522796c458588e4868692fb78dd824fc0fec65
Dima Brook <dima@xp.network>
2021-05-25 19:33:54 +0530

    elrond-mint: fix transfer event

--- elrond-mint-contract/src/events.rs
+++ elrond-mint-contract/src/events.rs
@@ -1,6 +1,10 @@
+use elrond_wasm::{String, api::BigUintApi};
 
-struct TransferEvent {
+elrond_wasm::derive_imports!();
+
+#[derive(TopEncode, TopDecode, TypeAbi)]
+struct TransferEvent<BigUint: BigUintApi> {
     action_id: String,
     to: String,
-    value: Balance
+    value: BigUint
 }
