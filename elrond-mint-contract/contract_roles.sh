59e65e9792366759da1f3a0ff402398bd8e4e2a8
Dima Brook <dima@xp.network>
2021-05-28 16:56:35 +0530

    fix contract_roles.sh

--- elrond-mint-contract/contract_roles.sh
+++ elrond-mint-contract/contract_roles.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+PROXY="http://localhost:7950"
 ADDR=$(erdpy wallet bech32 --decode $2)
 
 erdpy tx new --pem="./wallets/users/alice.pem" \
-    --data="setSpecialRole@$1@${ADDR}@45534454526F6C654C6F63616C4D696E74" \
+    --proxy=${PROXY} \
+    --data="setSpecialRole@$1@${ADDR}@45534454526F6C654C6F63616C4D696E74@45534454526F6C654C6F63616C4275726E" \
     --recall-nonce --receiver=erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u \
-    --gas-limit=500000000 --send
-
-erdpy tx new --pem="./wallets/users/alice.pem" \
-    --data="setSpecialRole@$1@${ADDR}@45534454526F6C654C6F63616C4275726E" \
-    --recall-nonce --receiver=erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u \
-    --gas-limit=500000000 --send
+    --gas-limit=500000000 --value 0 --send
