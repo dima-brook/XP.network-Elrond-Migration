43340af4615efc552ef329d2133ceed170a57aaf
VKint <dima@xp.network>
2021-06-02 13:48:00 +0530

    gitignore: add dist

--- validator/dist/consts.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-//export const ElrondTokenIdent = 'XPN-sdad';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uc3RzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbnN0cy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsNkNBQTZDIn0=
--- validator/dist/types.d.ts
+++
@@ -1,4 +0,0 @@
-import { AnyJson } from '@polkadot/types/types';
-export declare type ConcreteJson = {
-    readonly [index: string]: AnyJson;
-};
