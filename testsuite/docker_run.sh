2d2f5b01185a853ca9aa52e5b9571741341d6449
VKint <dima@xp.network>
2021-06-15 19:42:16 +0530

    testsuite: docker_run: start an observer node

--- testsuite/docker_run.sh
+++ testsuite/docker_run.sh
@@ -2,13 +2,16 @@
 
 cur=$PWD
 
-echo "Starting alice's substrate node(use ws port 9944 to access)"
+echo "Starting alice's substrate node"
 
-(&>/dev/null ~/node-template --chain local --base-path /tmp/alice --port 30333 --ws-port 9944 --rpc-port 9933 --node-key 0000000000000000000000000000000000000000000000000000000000000001 --validator --ws-external &)
+(&>/dev/null ~/node-template --alice --chain local --base-path /tmp/alice --port 30333 --ws-port 9945 --rpc-port 9933 --node-key 0000000000000000000000000000000000000000000000000000000000000001 --validator &)
 
-echo "Starting bob's substrate node(use ws port 9945 to access)"
+echo "Starting bob's substrate node"
 
-(&>/dev/null ~/node-template --chain local --base-path /tmp/bob --port 30334 --ws-port 9945 --rpc-port 9934 --validator --ws-external &)
+(&>/dev/null ~/node-template --bob --chain local --base-path /tmp/bob --port 30334 --ws-port 9946 --rpc-port 9934 --validator &)
+
+echo "Starting substrate observer node (use ws port 9944)"
+(&>/dev/null ~/node-template --chain local --tmp --ws-external --port 30335 --ws-port 9944 --rpc-port 9935 &)
 
 sleep 10
 
