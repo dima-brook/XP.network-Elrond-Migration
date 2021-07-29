51517bf5968d025517460ef175a584dbb1fc528c
rupansh <dima@xp.network>
2021-07-29 15:36:41 +0530

    elrond-event-middleware: fix cors

--- elrond-event-middleware/src/main.ts
+++ elrond-event-middleware/src/main.ts
@@ -7,15 +7,15 @@ const port = 6644;
 const app = express();
 const io = socket.socketMain();
 
+console.log("WARN: using permissive cors!!")
+
+app.use(cors({origin: '*'}))
+
 app.post('/event/transfer', (req, res) => {
     const id = BigInt(req.header("id"));
 
     io.emit("elrond:transfer_event", id.toString());
     res.send('{"status": "ok"}')
 });
-
-console.log("WARN: using permissive cors!!")
-
-app.use(cors())
 
 app.listen(port, () => console.log("Express Server is up!"))
